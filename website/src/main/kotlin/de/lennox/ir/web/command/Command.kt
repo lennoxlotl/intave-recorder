bde544c8b137bcdbd761795553b0cde63f0783b1
Lennox <lennox2508@gmail.com>
2021-09-11 23:55:57 +0200

    start improving webinterface code

--- website/src/main/kotlin/de/lennox/ir/web/command/Command.kt
+++ website/src/main/kotlin/de/lennox/ir/web/command/Command.kt
@@ -6,8 +6,18 @@ import de.lennox.ir.web.ANSI_RESET
 abstract class Command {
   private val subcommands = HashMap<String, (args: Array<out String>) -> Unit>()
 
+  /**
+   * Dispatches the command internally
+   *
+   * @param args The arguments for the command
+   */
   abstract fun dispatch(args: Array<out String>)
 
+  /**
+   * If required executes a valid subcommand or the basic command
+   *
+   * @param args The arguments for the command
+   */
   fun prepareDispatch(args: Array<out String>) {
     // Check for possible subcommands
     if (subcommands.isNotEmpty() && args.isNotEmpty()) {
@@ -24,11 +34,16 @@ abstract class Command {
     dispatch(args)
   }
 
+  /**
+   * Creates a new subcommand
+   *
+   * @param name The name of the subcommand, this also indicates how and when the command will be called
+   * @param func The function which will be called on subcommand dispatch
+   */
   fun subcommand(
     name: String,
     func: (args: Array<out String>) -> Unit
   ) {
     subcommands[name] = func
   }
-
 }
