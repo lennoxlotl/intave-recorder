2a5c0abc72610828e1d13cf77529157a77b73279
Lennox <lennox2508@gmail.com>
2021-09-12 00:39:15 +0200

    finish improving webinterface commands

--- website/src/main/kotlin/de/lennox/ir/web/command/CommandDispatcher.kt
+++ website/src/main/kotlin/de/lennox/ir/web/command/CommandDispatcher.kt
@@ -8,18 +8,25 @@ private const val PRE_FORMAT = "> "
 
 class CommandDispatcher : Runnable {
   override fun run() {
+    // Get all commands
     val commands = webinterface.commandRepository.repository
+    // Print the marker
     print(PRE_FORMAT)
     while (true) run loop@{
+      // Check if there was a console input
       val line = readLine()
       if (line != null) {
+        // Split the command input to receive valid arguments
         val args = line.split(" ").toTypedArray()
+        // Check if there is a valid command
         val command = commands[args[0]] ?: run {
           println("${ANSI_RED}Invalid command! Type 'help' for all commands.$ANSI_RESET")
           print(PRE_FORMAT)
           return@loop
         }
+        // Dispatch the command
         command.prepareDispatch(args.copyOfRange(1, args.size))
+        // Print the marker
         print(PRE_FORMAT)
       }
     }
--- website/src/main/kotlin/de/lennox/ir/web/command/PasswordCommand.kt
+++ website/src/main/kotlin/de/lennox/ir/web/command/PasswordCommand.kt
@@ -9,10 +9,14 @@ private val charPool: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
 class PasswordCommand : Command() {
 
   init {
+    // Create a subcommand for creating new passwords
     subcommand("add") { args ->
       if (args.size == 1) {
+        // Get the wanted id
         val id = args[0]
+        // Generate a password
         val generatedPassword = randomPassword(24)
+        // Create a password entity and push it into the selected database driver
         val passwordEntity = PasswordEntity(
           id,
           generatedPassword,
@@ -24,9 +28,12 @@ class PasswordCommand : Command() {
         println("${ANSI_RED}Syntax: password add <id>$ANSI_RESET")
       }
     }
+    // Create a subcommand for removing passwords
     subcommand("remove") { args ->
       if (args.size == 1) {
+        // Get the wanted id
         val id = args[0]
+        // Delete the password entity
         val passwordEntity = webinterface.driver.deletePasswordBy(id)
         val response = if (passwordEntity) {
           "${ANSI_GRAY}The password was deleted successfully!$ANSI_RESET"
@@ -38,13 +45,17 @@ class PasswordCommand : Command() {
         println("${ANSI_RED}Syntax: password remove <id>$ANSI_RESET")
       }
     }
+    // Create a subcommand for showing password information
     subcommand("show") { args ->
       if (args.size == 1) {
+        // Get the wanted id
         val id = args[0]
+        // Check if there is a valid password entity
         val passwordEntity = webinterface.driver.passwordByPassword(id) ?: run {
           println("${ANSI_RED}This password could not be found!${ANSI_GRAY}")
           return@subcommand
         }
+        // Print all information about the requested password entity
         println("${ANSI_GRAY}Password Information (${ANSI_RED}$id${ANSI_GRAY}):")
         println("  - Password: ${passwordEntity.password}")
         println("  - Linked fingerprint: ${passwordEntity.linkedFingerprint}$ANSI_RESET")
@@ -55,12 +66,19 @@ class PasswordCommand : Command() {
   }
 
   override fun dispatch(args: Array<out String>) {
+    // Default help command
     println("${ANSI_RED}Password Command$ANSI_GRAY:")
     println("  - password add <id> (creates a password with an identifier, the identifier is used to delete it or to get information about it)")
     println("  - password remove <id> (deletes a password)")
     println("  - password show <id> (shows information about a password)")
   }
 
+  /**
+   * Generates a password randomly
+   *
+   * @param length The length of the password
+   * @return The generated password
+   */
   private fun randomPassword(length: Int): String {
     return (1..length)
       .map { Random.nextInt(0, charPool.size) }
